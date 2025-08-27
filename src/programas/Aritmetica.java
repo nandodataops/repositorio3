d1a34599f6ae322a6fe2c26dd51738e29a118603
Fernando <Fernando@pc-winserver.tailecb83b.ts.net.>
2025-08-26 20:44:01 -0500

    Carga inicial

---
+++ src/programas/Aritmetica.java
@@ -0,0 +1,23 @@
+
+package programas;
+import java.util.Scanner;
+public class Aritmetica {
+    public static void main(String[] args){
+        //declarando vareiables
+        double numero1, numero2, suma, promedio;
+        Scanner lectura=new Scanner(System.in);
+        //entrada de datos
+        System.out.println("Ingresa el numero 1: ");
+        numero1=lectura.nextDouble();
+        System.out.println("Ingresa el numero 2: ");
+        numero2=lectura.nextDouble();
+        //proceso de datpos
+        suma=numero1+numero2;
+        promedio=suma/2;
+        //salida de datos
+        System.out.println("La suma es: "+suma);
+        System.out.println("El promedio es: "+promedio);
+
+
+    }
+}
