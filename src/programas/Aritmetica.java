70aa251f0d45ebf83da796e789d3d3be08ba1fea
Fernando <Fernando@pc-winserver.tailecb83b.ts.net.>
2025-08-26 22:09:22 -0500

    Carga inicial

--- src/programas/Aritmetica.java
+++ src/programas/Aritmetica.java
@@ -11,7 +11,8 @@ public class Aritmetica {
         numero1=lectura.nextDouble();
         System.out.println("Ingresa el numero 2: ");
         numero2=lectura.nextDouble();
-        System.out.println("Resultados de la aplicacion");        
+        System.out.println("Resultados de la aplicacion");       
+        System.out.println("Resultados de la aplicacion 2");  
         //proceso de datpos
         suma=numero1+numero2;
         promedio=suma/2;
